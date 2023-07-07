b7eec28e642ff2e2be8dca0c69a61c606a95aab5
Anurag <anuragdas12921@gmail.com>
2023-07-07 13:24:10 +0530

    Added Factorial And Percentage Functionalities

--- Mini Calculator/script.js
+++ Mini Calculator/script.js
@@ -6,11 +6,32 @@ var operator = null;
 function ShowNumber(value) {
   dis.textContent += value;
 }
+function FactOp(op1){
+ 
+  if(op1===0 || op1===1)
+  {
+    return 1;
+  }
+  
+  res=op1*FactOp(op1-1);
+
+  return res;
+}
 function handleOp(value) {
   operator = value;
   op1 = parseFloat(dis.textContent);
   dis.innerText = "";
+  if(value==="%")
+  {
+    var res=op1/100;
+    dis.innerText=res;
+  }
+  if(value==="!"){
+    var res=FactOp(op1);
+    dis.innerText=res;
+  }
 }
+
 function performOp() {
   if (operator === "+") {
     op2 = parseFloat(dis.textContent);
@@ -32,6 +53,12 @@ function performOp() {
     var res = op1 / op2;
     dis.innerText = res;
   }
+  // if(operator==="!")
+  // {
+  //   op1=parseFloat(dis.textContent);
+  //   var res=FactOp(op1);
+  //   dis.innerText=res;
+  // }
 }
 function clearDisplay() {
   dis.innerText = "";
@@ -42,9 +69,12 @@ function clearDisplay() {
 function Show() {
   var value = this.getAttribute("data-value");
 
-  if (value === "=") {
+  if(value==="!"){
+    handleOp(value);
+  }
+  else if (value === "=") {
     performOp();
-  } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+  } else if (value === "+" || value === "-" || value === "*" || value === "/" || value==="%") {
     handleOp(value);
   } else if (value === "AC") {
     clearDisplay();
