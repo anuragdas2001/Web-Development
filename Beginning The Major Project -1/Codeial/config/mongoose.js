391890089095a46079b65900a3c479633fd689b7
Anurag Das <anuragdas12921@gmail.com>
2023-09-03 00:59:35 +0530

    Added DB in Codeial

---
+++ Beginning The Major Project -1/Codeial/config/mongoose.js
@@ -0,0 +1,22 @@
+//require the library
+const mongoose = require("mongoose");
+
+//connect the database
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//acquire the connection to check if it is succesful
+const db = mongoose.connection;
+
+//error
+// db.on("error", console.error.bind(console, "error connecting to db"));
+// db.on('error', err => console.error('Connection error:', err));
+
+db.on('error',function(error){
+    console.error('Connection Establishment Failed:',error);
+});
+
+//up and running then print the message
+db.once('open', function () {
+  console.log("Connection Established Successfully!");
+});
+
--- Beginning The Major Project -1/Codeial/index.js
+++ Beginning The Major Project -1/Codeial/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
-
+const DB=require('./config/mongoose');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
