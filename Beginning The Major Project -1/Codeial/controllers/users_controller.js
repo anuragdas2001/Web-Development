4fcd1a89bd51c463ff5080620bfa25b1d89fa77f
Anurag Das <anuragdas12921@gmail.com>
2023-09-03 20:02:25 +0530

    Added SignIn & SignUp Page

--- Beginning The Major Project -1/Codeial/controllers/users_controller.js
+++ Beginning The Major Project -1/Codeial/controllers/users_controller.js
@@ -1,5 +1,31 @@
+const { modelName } = require("../models/user");
+
 module.exports.profile = function(req,res){
     return res.render("users",{
         title:"Users",
     });
+}
+
+//render the sign up page
+module.exports.signup=function(req,res){
+    return res.render("user_sign_up",{
+        title: "Codeial | Sign Up"
+    });
+}
+
+//render the sign in page
+module.exports.signin=function(req,res){
+    return res.render("user_sign_in",{
+        title: "Codeial | Sign In"
+    });
+}
+
+//get the sign up data
+module.exports.create=function(req,res){
+
+}
+
+//sign in and create a session for the user
+module.exports.create_session=function(req,res){
+
 }
--- Beginning The Major Project -1/Codeial/routes/users.js
+++ Beginning The Major Project -1/Codeial/routes/users.js
@@ -7,6 +7,8 @@ const usercontroller = require('../controllers/users_controller');
 console.log('Users Loaded');
 
 router.get('/profile',usercontroller.profile);
+router.get('/Sign-up',usercontroller.signup);
+router.get('/Sign-in',usercontroller.signin);
 
 
 module.exports=router;
