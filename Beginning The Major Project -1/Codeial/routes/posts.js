894acd770085c475df885ef5425aa215136ed0e2
Anurag Das <anuragdas12921@gmail.com>
2023-09-23 18:14:53 +0530

    Added Authentication for posts

--- Beginning The Major Project -1/Codeial/routes/posts.js
+++ Beginning The Major Project -1/Codeial/routes/posts.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('passport');
 const postcontroller = require('../controllers/post_controller');
 
 
-router.post('/create-post',postcontroller.create_post);    
+router.post('/create-post',passport.checkAuthentication,postcontroller.create_post);    
 
 
 module.exports=router;
