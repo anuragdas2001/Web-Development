6e4f0db4ac6218bb972ed229a558b30e9c7f65b1
Anurag Das <anuragdas12921@gmail.com>
2023-10-10 00:04:56 +0530

    Added HTML email template for posts

--- Beginning The Major Project -1/Codeial/mailers/posts_mailer.js
+++ Beginning The Major Project -1/Codeial/mailers/posts_mailer.js
@@ -9,11 +9,13 @@ const nodemailer = require('../config/nodemailer');
 // this is another way of exporting a method
 exports.newPost = (post) => {
     console.log('Inside newPost mailer',post);
+ let htmlString = nodemailer.renderTemplate({post:post}, '/posts/new_posts.ejs');
+    
     nodemailer.transporter.sendMail({
         from: 'anuragdas12921@gmail.com',
         to: post.user.email,
         subject: 'New Post Published',
-        html:'<h1>Yup,your Post is now published!</h1>'
+        html:htmlString 
     },(error,info)=>{
         if(error){
             console.log('Error in sending mail',error);
