42238c4e496fc26d9e7469b22e146c184f33e926
Anurag Das <anuragdas12921@gmail.com>
2023-10-12 21:48:00 +0530

    Separated Reset Password & Forgot Password

--- Beginning The Major Project -1/Codeial/workers/reset_email_worker.js
+++ Beginning The Major Project -1/Codeial/workers/reset_email_worker.js
@@ -1,12 +1,11 @@
-const queue = require('../config/kue');
+const queue = require("../config/kue");
 
-const reset_email_mailer = require('../mailers/Forgot_Password');
+const reset_email_mailer = require("../mailers/reset_password");
 
+queue.process("reset", function (job, done) {
+  console.log("reset worker is processing a job", job.data);
 
-queue.process('reset',function(job,done){
-    console.log('reset worker is processing a job',job.data);
+  reset_email_mailer.reset_Password(job.data);
 
-    comments_email_mailer.newComment(job.data);
-
-    done();
-})
+  done();
+});
