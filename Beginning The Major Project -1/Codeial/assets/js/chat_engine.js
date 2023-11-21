857783ac482e82bba8975f71da27088a7a2cf102
anuragdas2001 <anuragdas12921@gmail.com>
2023-11-21 13:12:03 +0530

    Fixed Bugs with Chat Engine

--- Beginning The Major Project -1/Codeial/assets/js/chat_engine.js
+++ Beginning The Major Project -1/Codeial/assets/js/chat_engine.js
@@ -10,8 +10,64 @@ class ChatEngine{
         }
     }
     connectionhandler(){
+
+        let self = this;
+
+
         this.socket.on('connect',function(){
             console.log('Connection established using socket....!');
+
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log('A user joined!',data);
+                
+
+            })
+        });
+
+        //Send a message on clicking the send message button
+        $('#send-button').click(function(e){
+            e.preventDefault();
+            console.log('Send Button CLicked!');
+            let msg = $('#chat-message-input').val();
+
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+                
+            }
+        
+        
+        });
+
+        self.socket.on('receive_message',function(data){
+            console.log('message recieved',data.message);
+           
+            let newMessage = $('<li>');
+            let messageType = 'other-message';
+
+            if(data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+            newMessage.append($('<span>',{
+                'html':data.message
+            }));
+
+            // newMessage.append($('<sub>',{
+            //     'html':data.user_email
+            // }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
         })
+
     }
 }
--- Beginning The Major Project -1/Codeial/config/chat_sockets.js
+++ Beginning The Major Project -1/Codeial/config/chat_sockets.js
@@ -12,5 +12,20 @@ module.exports.chatSockets = function(socketServer){
         socket.on('disconnect',function(){
             console.log('Socket Disconnected....!');
         })
+
+        socket.on('join_room',function(data){
+            console.log('join request received',data);
+            socket.join(data.chatroom);
+
+            //emiting in a specific chatroom
+            io.in(data.chatroom).emit('user_joined',data);
+        });
+
+        socket.on('send_message',function(data){
+            console.log('message received ....',data.message);
+            io.in(data.chatroom).emit('receive_message',data);
+        });
+
+
     });
 }
