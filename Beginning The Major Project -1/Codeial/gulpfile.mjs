3fe88f17da3d315f79505b139715ff5784f0b3aa
Anurag Das <anuragdas12921@gmail.com>
2023-12-23 23:47:26 +0530

    Fixed Bugs and Disabled Gulp

--- Beginning The Major Project -1/Codeial/gulpfile.mjs
+++ Beginning The Major Project -1/Codeial/gulpfile.mjs
@@ -1,70 +1,70 @@
-// const gulp = require('gulp');
-// const cssnano = require('gulp-cssnano');
+const gulp = require('gulp');
 
-// const rev = require('gulp-rev');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 
-// import gulp from 'gulp';
-// import cssnano from 'gulp-cssnano';
-// import rev from 'gulp-rev';
 
-// gulp.task('css',()=>{
-//     console.log('minifying css ...');
-//     gulp.src('./assets/css')
-//     .pipe(css())
-//     .pipe(cssnano())
-//     .pipe(gulp.dest('./assets.css'));
 
-//     return gulp.src('./assets/**/*.css')
-//     .pipe(rev())
-//     .pipe(gulp.dest('./public/assets'))
-//     .pipe(rev.manifest({
-//         cwd:'public',
-//         merge:true
-//     }))
-//     .pipe(gulp.dest('./public/assets'));
-// });
+gulp.task('css', function(done){
+    console.log('minifying css...');
+    gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./assets.css'));
 
-// Import modules using ES module syntax
-// import gulp from 'gulp';
-// import cssnano from 'gulp-cssnano'; 
-// import rev from 'gulp-rev';
+     gulp.src('./assets/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
 
-// // Export task function  
-// export function css() {
-//   return gulp.src('./assets/**/*.css')
-//     .pipe(cssnano())
-//     .pipe(gulp.dest('./public/assets')) 
-//     .pipe(rev())
-//     .pipe(gulp.dest('./public/assets'))
-//     .pipe(rev.manifest({
-//       cwd: 'public',
-//       merge: true
-//     }))
-//     .pipe(gulp.dest('./public/assets'));
-// }
 
-// // Define task using gulp API
-// gulp.task('css', gulp.series(css));
+gulp.task('js', function(done){
+    console.log('minifying js...');
+     gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done()
+});
 
-// gulpfile.mjs
 
-import gulp from 'gulp';
-import cssnano from 'gulp-cssnano';
-import rev from 'gulp-rev';
+gulp.task('images', function(done){
+    console.log('compressing images...');
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
 
-gulp.task('css', () => {
-    console.log('minifying css ...');
 
-    gulp.src('./assets/css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('./assets.css'));
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+    del.sync('./public/assets');
+    done();
+});
 
-    return gulp.src('./assets/**/*.css')
-        .pipe(rev())
-        .pipe(gulp.dest('./public/assets'))
-        .pipe(rev.manifest({
-            cwd: 'public',
-            merge: true
-        }))
-        .pipe(gulp.dest('./public/assets'));
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets');
+    done();
 });
